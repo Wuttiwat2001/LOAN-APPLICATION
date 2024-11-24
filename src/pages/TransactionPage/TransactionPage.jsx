163870a480f7806416be9532e0b07d99e4f98178
KhunCaptain <captenlnw_za@hotmail.com>
2024-11-25 01:28:34 +0700

    transaction page

---
+++ src/pages/TransactionPage/TransactionPage.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Table } from "antd";
+import * as transactionAction from "../../redux/actions/transaction.action";
+
+const columns = [
+  {
+    title: "คู่สัญญา",
+    dataIndex: "counterparty",
+    key: "counterparty",
+  },
+  {
+    title: "ประเภท",
+    dataIndex: "type",
+    key: "type",
+  },
+  {
+    title: "จำนวน",
+    key: "amount",
+    dataIndex: "amount",
+  },
+  {
+    title: "วันที่สร้าง",
+    key: "createdAt",
+    dataIndex: "createdAt",
+  },
+];
+
+const TransactionPage = () => {
+  const transactionReducer = useSelector((state) => state.transactionReducer);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(transactionAction.loadTransaction());
+  }, [dispatch]);
+  
+  const dataSourceWithKeys = transactionReducer.transactions.map((transaction, index) => ({
+    ...transaction,
+    key: transaction.id 
+  }));
+
+  return (
+    <>
+      <Table columns={columns} dataSource={dataSourceWithKeys} />
+    </>
+  );
+};
+
+export default TransactionPage;
