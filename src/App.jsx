06df19bcb44c45887f9e9c47b65541ac0b2ad5d7
KhunCaptain <captenlnw_za@hotmail.com>
2024-11-24 21:42:35 +0700

    check restore login hide menu hide header

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,35 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router } from 'react-router-dom';
+import React, { useState,useEffect } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import { Layout } from "antd";
 import AppHeader from "./components/AppHeader";
 import AppMenu from "./components/AppMenu";
 import AppContent from "./components/AppContent";
 
+import { useSelector, useDispatch } from "react-redux";
+import * as loginAction from "./redux/actions/login.action";
+
 const App = () => {
   const [collapsed, setCollapsed] = useState(false);
+  const loginReducer = useSelector((state) => state.loginReducer);
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(loginAction.restoreLogin());
+  }, []);
+  
   return (
     <Router>
-    <div style={{ height: "100vh" }}>
-      <Layout style={{ height: "100%" }}>
-        <AppMenu collapsed={collapsed} />
-        <Layout>
-          <AppHeader collapsed={collapsed} setCollapsed={setCollapsed} />
-          <AppContent />
+      <div style={{ height: "100vh" }}>
+        <Layout style={{ height: "100%" }}>
+          {loginReducer.isLogin && <AppMenu collapsed={collapsed} />}
+          <Layout>
+            {loginReducer.isLogin && (
+              <AppHeader collapsed={collapsed} setCollapsed={setCollapsed} />
+            )}
+            <AppContent />
+          </Layout>
         </Layout>
-      </Layout>
-    </div>
+      </div>
     </Router>
   );
 };
