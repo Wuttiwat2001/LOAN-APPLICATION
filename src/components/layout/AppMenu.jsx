0b234f408596db2670565b1f1e7b699e6e4e2d9e
KhunCaptain <captenlnw_za@hotmail.com>
2024-11-25 10:46:18 +0700

    check key select menu when refresh

--- src/components/layout/AppMenu.jsx
+++ src/components/layout/AppMenu.jsx
@@ -1,21 +1,16 @@
 import React from "react";
 import { Layout, Menu } from "antd";
 import { UserOutlined, VideoCameraOutlined, UploadOutlined } from "@ant-design/icons";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const { Sider } = Layout;
 
 const AppMenu = ({ collapsed }) => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   const handleMenuClick = (e) => {
-    if (e.key === "1") {
-      navigate("/");
-    } else if (e.key === "2") {
-      navigate("/about");
-    } else if (e.key === "3") {
-      navigate("/transaction");
-    }
+    navigate(e.key);
   };
 
   return (
@@ -23,26 +18,26 @@ const AppMenu = ({ collapsed }) => {
       <div className="demo-logo-vertical" />
       <Menu
         mode="inline"
-        defaultSelectedKeys={["1"]}
+        selectedKeys={[location.pathname]}
         onClick={handleMenuClick}
         style={{
-          height: '100%',
+          height: "100%",
         }}
         items={[
           {
-            key: "1",
+            key: "/",
             icon: <UserOutlined />,
             label: "Home",
           },
           {
-            key: "2",
+            key: "/about",
             icon: <VideoCameraOutlined />,
             label: "About",
           },
           {
-            key: "3",
+            key: "/transaction",
             icon: <UploadOutlined />,
-            label: "Upload",
+            label: "Transaction",
           },
         ]}
       />
