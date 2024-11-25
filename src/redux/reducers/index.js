96eaf4ebc0e547ab4db58c42ab81dc1fabf7e259
KhunCaptain <captenlnw_za@hotmail.com>
2024-11-25 16:07:03 +0700

    export user reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "@reduxjs/toolkit";
 import loginReducer from "./login.reducer";
 import registerReducer from "./register.reducer";
 import transactionReducer from "./transaction.reducer";
+import userReducer from "./user.reducer";
 
 const rootReducer = combineReducers({
   loginReducer,
   registerReducer,
-  transactionReducer
+  transactionReducer,
+  userReducer
 });
 
 export default rootReducer;
