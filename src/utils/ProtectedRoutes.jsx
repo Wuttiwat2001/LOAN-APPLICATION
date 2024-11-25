ac9811b4c0db7d4bb008d9cd1cfa5423fc3ec868
KhunCaptain <captenlnw_za@hotmail.com>
2024-11-25 10:41:33 +0700

    fix protected routes and public routes

--- src/utils/ProtectedRoutes.jsx
+++ src/utils/ProtectedRoutes.jsx
@@ -1,8 +1,28 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
+import { Spin } from "antd";
+
 
 const ProtectedRoutes = () => {
   const isLogin = useSelector((state) => state.loginReducer.isLogin);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 200); 
+
+    return () => clearTimeout(timer);
+  }, [isLogin]);
+
+  if (loading) {
+    return (
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+     <Spin  size="large"  />
+      </div>
+    );
+  }
 
   return isLogin ? <Outlet /> : <Navigate to="/login" />;
 };
