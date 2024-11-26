b17fa68f48ab2ed56256df314485285ce405638d
KhunCaptain <wuttiwat.p@poontana.co.th>
2024-11-26 15:52:36 +0700

    clear code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import  { useState,useEffect } from "react";
 import { Layout } from "antd";
 import AppHeader from "./components/layout/AppHeader";
 import AppMenu from "./components/layout/AppMenu";
