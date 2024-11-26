4f1c4a023b10dca00f03676585fafe8b5c6b8603
KhunCaptain <captenlnw_za@hotmail.com>
2024-11-26 22:12:54 +0700

    combine reducers and create constans for transaction sender borrow

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,21 +2,24 @@ import { combineReducers } from "@reduxjs/toolkit";
 import loginReducer from "./login.reducer";
 import registerReducer from "./register.reducer";
 import transactionReducer from "./transaction.reducer";
+import transactionSenderBorrowReducer from "./transactionSenderBorrow.reducer";
 import userReducer from "./user.reducer";
 import createRequestBorrowReducer from "./createRequestBorrow.reducer";
 import requestSenderReducer from "./requestSender.reducer";
 import requestReceiverReducer from "./requestReceiver.reducer";
 import requestEditReducer from "./requestEdit.reducer";
 
+
 const rootReducer = combineReducers({
   loginReducer,
   registerReducer,
   transactionReducer,
+  transactionSenderBorrowReducer,
   userReducer,
   createRequestBorrowReducer,
   requestSenderReducer,
   requestReceiverReducer,
-  requestEditReducer
+  requestEditReducer,
 });
 
 export default rootReducer;
